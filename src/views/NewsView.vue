684d0d289ed9aa21101003a24bfef4deff1beceb
ygtdev <51426246+ygtdev@users.noreply.github.com>
2023-02-22 22:26:24 +0300

    Update NewsView.vue

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -2,8 +2,13 @@
   <div class="mt-32 flex flex-col items-center justify-center">
     <h1 class="text-5xl font-bold">Haberler</h1>
     <p>(ben daha izlemem aga.)</p>
-    <div class="mt-6">
-      <img src="/instagram_dm.png" alt="DM" />
+    <div
+      class="mt-6 flex flex-col items-center gap-2 bg-gray-200 p-4 rounded-xl"
+    >
+      <h1 class="font-bold text-lg">
+        Alkol içtin birşey demedik ama bu son yaptığına sessiz kalmayacağız!!
+      </h1>
+      <img src="/instagram_dm.png" alt="DM" class="w-80" />
     </div>
   </div>
 </template>
